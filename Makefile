4ff9f816b656e44d521501ec3a28f854f57a25c4
DrachovDmytro <dmytro.drachov@gmail.com>
2021-07-03 02:56:02 +0300

    [semver:minor] feat: add preamble for bash. refactore. update npm_module format (#9)

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
-.PHONY: replace_lib
+.PHONY: replace_scripts
 
 SHELL  := /bin/bash
 __LIBRARY_PLACEHOLDER__=`cat lib/index.js`
+__INSTALL_NPM_MODULES_PLACEHOLDER__=`cat lib/install_npm_modules.js`
+__WRAPPER_PLACEHOLDER__=`cat lib/wrapper.js`
 
-replace_lib:
-	@export __LIBRARY_PLACEHOLDER__=${__LIBRARY_PLACEHOLDER__}; \
-	envsubst '$${__LIBRARY_PLACEHOLDER__}' < ./templates/script.sh.tpl > ./src/scripts/script.sh
+replace_scripts:
+	@export __LIBRARY_PLACEHOLDER__=${__LIBRARY_PLACEHOLDER__} && \
+	export __INSTALL_NPM_MODULES_PLACEHOLDER__=${__INSTALL_NPM_MODULES_PLACEHOLDER__} && \
+	export __WRAPPER_PLACEHOLDER__=${__WRAPPER_PLACEHOLDER__} && \
+	envsubst '\
+		$${__LIBRARY_PLACEHOLDER__}\
+		$${__INSTALL_NPM_MODULES_PLACEHOLDER__}\
+		$${__WRAPPER_PLACEHOLDER__}\
+	' < ./templates/script.template.sh > ./src/scripts/script.sh
--- src/scripts/script.sh
+++ src/scripts/script.sh
@@ -1,6 +1,6 @@
-SetupLibrary() {
+SetupModules() {
 cat <<- "EOF" > "$TMP_DIR"/index.js
-const { exec } = require('child_process')
+  const { exec } = require('child_process')
 const { once } = require('events')
 const MB = 1024 * 1024
 
@@ -28,6 +28,16 @@ const shell = (strs, ...args) => {
   return true
 }
 
+let defaultShellPreamble = 'set -o errexit -o pipefail -o nounset;'
+const shellPreamble = (strs, ...args) => {
+  const newShell = strs
+  .map(s => `${s}${args.shift() || ''}`)
+  .join('')
+  .replace(/\n/g, '\\\n')
+  defaultShellPreamble = newShell
+  return true
+}
+
 let cmdCount = 0
 /**
  * @param {object} params
@@ -35,10 +45,11 @@ let cmdCount = 0
  */
 const bash = (params) => {
   return (strs, ...args) => {
-    const cmd = strs
+    let cmd = defaultShellPreamble + strs
       .map(s => `${s}${args.shift() || ''}`)
       .join('')
-      .replace(/\n/g, '\\\n');
+      .replace(/\n/g, '\\\n')
+      .replace(/\$\(/gm, `$(${defaultShellPreamble}`);
     cmdCount += 1
 
     const options = { shell: defaultShell, cwd: defaultCwd, maxBuffer: 50 * MB }
@@ -48,8 +59,11 @@ const bash = (params) => {
       child.on('error', reject);
       child.on('exit', (exitCode) => {
         promise.exitCode = exitCode
+        promise.stdout = promise.stdout.replace(/\n$/g, '')
+        promise.stderr = promise.stderr.replace(/\n$/g, '')
+
         const result = {
-          stdout: promise.stdout,
+          stdout: promise.stdout.replace(/\n$/g, ''),
           stderr: promise.stderr,
           exitCode: exitCode
         }
@@ -85,43 +99,87 @@ global.$ = bash({ autoFail: true })
 global.$$ = bash({ autoFail: false })
 global.EE = exportEnv
 global.stopJob = stopJob
-global.cwd = cwd
+global.$cwd = cwd
 global.shell = shell
+global.$preamble = shellPreamble
 EOF
+cat <<- "EOF" > "$TMP_DIR"/install_npm_modules.js
+  const { execSync } = require('child_process')
+const modules = process.env.NPM_MODULES
+    .split('\n')
+    .filter(l => l.trim() !== '')
+    .map(m => {
+        let [module, alias = ''] = m.split('#');
+        let [moduleName = '', version = ''] = module.split('@')
+        alias = alias.trim() === '' ? moduleName : alias.trim()
+        moduleName = moduleName.trim()
+        version = version.trim() === '' ? 'latest' : version.trim()
+        if(moduleName === '') {
+            console.error('No module name provided')
+            return process.exit(1)
+        }
+        return { moduleName, alias, version }
+    })
+    .reduce((acc, { moduleName, version }) => acc += `${moduleName}@${version}`, '')
+
+if (modules === '') process.exit(0)
+execSync(`npm install --no-package-lock --prefix ${process.env.TMP_DIR} ${modules}`)
+EOF
+  node "$TMP_DIR"/install_npm_modules.js
 }
 
 Run() {
-WRAPPER=$(cat << END
-(async () => {
-    $SCRIPT
+cat <<- "EOF" > "$TMP_DIR"/wrapped.js
+  (async () => {
+  const modules = process.env.NPM_MODULES
+  modules
+    .split('\n')
+    .filter(l => l.trim() !== '')
+    .map(m => {
+        let [module, alias = ''] = m.split('#');
+        let [moduleName = ''] = module.split('@')
+        alias = alias.trim() === '' ? moduleName : alias.trim()
+        moduleName = moduleName.trim()
+        if(moduleName === '') {
+            console.error('No module name provided')
+            return process.exit(1)
+        }
+        eval(`global['${alias}'] = require('${moduleName}')`)
+    })
+    eval(`
+      (async () => {
+        ${process.env.SCRIPT}
+      })().catch((err) => {
+        console.log(err);
+        process.exit(1);
+      }).then(() => {
+        setTimeout(() => {
+          process.exit(0);
+        }, 1000).unref();
+      })
+    `)
 })().catch((err) => {
-    console.log(err);
-    process.exit(1);
-}).then(() => {
-    setTimeout(() => {
-        process.exit(0);
-    }, 1000);
+  console.log(err);
+  process.exit(1);
 })
-END
-)
-    node -r "$TMP_DIR/index.js" -e "$WRAPPER"
-}
+EOF
 
-SetupModules() {
-    export NODE_PATH=$TMP_DIR/node_modules
-    MODULES=$(echo "$NPM_MODULES" | sed -r 's/[;]+/ /g')
-    # shellcheck disable=SC2086
-    npm install --no-package-lock --prefix "$TMP_DIR" $MODULES
+  WRAPPED_SCRIPT=$(cat "$TMP_DIR"/wrapped.js)
+
+  # shellcheck disable=SC2016
+  echo "Script to be RUN:"
+  echo "$WRAPPED_SCRIPT"
+  node -r "$TMP_DIR/index.js" -e "$WRAPPED_SCRIPT"
 }
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-    TMP_DIR=$(mktemp -d -t ci-XXXXXXXXXX)
-    SetupLibrary
-    SetupModules
-    echo "Script to be RUN"
-    echo "$SCRIPT"
-    Run
+  TMP_DIR=$(mktemp -d -t ci-XXXXXXXXXX)
+  NODE_PATH=$TMP_DIR/node_modules
+  export NODE_PATH
+  export TMP_DIR
+  SetupModules
+  Run
 fi
