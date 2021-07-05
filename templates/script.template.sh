a9af5f3795187f708bf4e35b1400a336f1b46649
Dmytro Drachov <dmytro.drachov@gmail.com>
2021-07-05 23:30:15 +0300

    fix: allow to require scoped/org modules

--- templates/script.template.sh
+++ templates/script.template.sh
@@ -2,6 +2,9 @@ SetupModules() {
 cat <<- "EOF" > "$TMP_DIR"/index.js
   ${__LIBRARY_PLACEHOLDER__}
 EOF
+cat <<- "EOF" > "$TMP_DIR"/parse_modules.js
+  ${__PARSE_MODULES_PLACEHOLDER__}
+EOF
 cat <<- "EOF" > "$TMP_DIR"/install_npm_modules.js
   ${__INSTALL_NPM_MODULES_PLACEHOLDER__}
 EOF
@@ -25,8 +28,10 @@ EOF
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-  TMP_DIR=$(mktemp -d -t ci-XXXXXXXXXX)
-  NODE_PATH=$TMP_DIR/node_modules
+  TMP_ROOT=$(dirname "$(mktemp -u)")
+  TMP_DIR="${TMP_ROOT}/ci-node-run-temp"
+  mkdir -p "$TMP_DIR"
+  NODE_PATH="${TMP_DIR}/node_modules"
   export NODE_PATH
   export TMP_DIR
   SetupModules
